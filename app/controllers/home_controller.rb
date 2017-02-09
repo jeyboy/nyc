af7855b41b3e8856d72591aa3c30ae0dc17fbd77
JB <jeyboy@bigmir.net>
2017-02-09 06:26:56 -0800

    form fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
     @min_year = EnergyUsage.minimum(:year)
     @max_year = EnergyUsage.maximum(:year)
     @energy_usages = Building.joins(energy_usages: :measurement)
-    @energy_usages = @energy_usages.where(year: @year) if @year
+    @energy_usages = @energy_usages.where(energy_usages: {year: @year}) if @year
 
     @energy_usages = @energy_usages.page(params[:page]).per(50)
   end

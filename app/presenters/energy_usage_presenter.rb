dd05f18ae086c2a6c91373b623603f21ad0cd9f1
JB <jeyboy@bigmir.net>
2017-02-09 12:02:09 -0800

    update for data output

--- app/presenters/energy_usage_presenter.rb
+++ app/presenters/energy_usage_presenter.rb
@@ -11,35 +11,15 @@ class EnergyUsagePresenter
     @max_year = EnergyUsage.maximum(:year)
     @year = params[:year] || @min_year
 
-    @energy_usages = Building.joins(:energy_usages).where(energy_usages: {year: @year})
-    @energy_usages = @energy_usages.order('buildings.name','energy_usages.measurement_id')
-       .group('buildings.name','energy_usages.measurement_id')
-       .sum(:amount)
-
-    @id_relations = {}
-    @measurements = Measurement.all.each_with_object({}) do |measure, res|
-      res[measure.id] = measure.name
-      @id_relations[measure.id] = @id_relations.size
+    @energy_usages = Building.joins(energy_usages: :measurement).where(energy_usages: {year: @year})
+                         .order('buildings.name','measurements.name')
+                         .group('buildings.name','measurements.name')
+                         .sum(:amount)
+    @energy_usages = @energy_usages.each_with_object({}) do |item, hash|
+      hash[item.first.first]||={}
+      hash[item.first.first][item.first.last]=item.last
     end
-    @energy_usages = @energy_usages.group_by {|val| [val[0][0], val[0][2]]}
-  end
 
-  def each_measurement
-    raise LocalJumpError unless block_given?
-
-    @measurements.each_pair do |_, name|
-      yield name
-    end
-  end
-
-  def each_building_attrs
-    raise LocalJumpError unless block_given?
-    @energy_usages.each_pair do |k, v|
-      attrs = v.each_with_object([0.0] * @measurements.size) do |val, res|
-        res[@id_relations[val[0][1]]] = ActionController::Base.helpers.number_to_currency(val[1])
-      end
-
-      yield k[0], @year, attrs
-    end
+    @measurements = Measurement.all.map(&:name)
   end
 end

43f89229cc6aa22b53349655690d89080b1d1617
JB <jeyboy@bigmir.net>
2017-02-09 06:02:19 -0800

    add base pagination

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   def index
+    @year = params[:year]
+    @min_year = EnergyUsage.minimum(:year)
+    @max_year = EnergyUsage.maximum(:year)
+    @energy_usage = Building.joins(energy_usages: :measurement)
+    @energy_usage = @energy_usage.where(energy_usages: {year: @year}) if @year
 
+    @energy_usage = @energy_usage.per(50).page params[:page]
   end
 end

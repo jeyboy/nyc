994b2a1e583e31805916e8bd0b42c2d72caa726d
JB <jeyboy@bigmir.net>
2017-02-09 09:55:14 -0800

    refactor for data output

--- app/presenters/energy_usage_presenter.rb
+++ app/presenters/energy_usage_presenter.rb
@@ -3,8 +3,8 @@ class EnergyUsagePresenter
   class_attribute :energy_usages
   class_attribute :min_year
   class_attribute :max_year
-
   class_attribute :measurements
+  class_attribute :id_relations
 
   def initialize(params)
     @min_year = EnergyUsage.minimum(:year)
@@ -16,8 +16,11 @@ class EnergyUsagePresenter
        .group('buildings.name','energy_usages.measurement_id')
        .sum(:amount)
 
-    @measurements = Measurement.all.each_with_object({}) { |measure, res| res[measure.id] = measure.name }
-
+    @id_relations = {}
+    @measurements = Measurement.all.each_with_object({}) do |measure, res|
+      res[measure.id] = measure.name
+      @id_relations[measure.id] = @id_relations.size
+    end
     @energy_usages = @energy_usages.group_by {|val| [val[0][0], val[0][2]]}
   end
 
@@ -32,16 +35,8 @@ class EnergyUsagePresenter
   def each_building_attrs
     raise LocalJumpError unless block_given?
     @energy_usages.each_pair do |k, v|
-      i = 0
-      attrs = @measurements.each_with_object([]) do |(mid, _), res|
-        pos_id = (v[i][0][1] rescue -1)
-
-        if mid < pos_id
-          res << 0.0
-        else
-          res << ((v[i][0][1] rescue -1) == mid ? ActionController::Base.helpers.number_to_currency(v[i][1]) : 0.0)
-          i += 1
-        end
+      attrs = v.each_with_object([0.0] * @measurements.size) do |val, res|
+        res[@id_relations[val[0][1]]] = ActionController::Base.helpers.number_to_currency(val[1])
       end
 
       yield k[0], @year, attrs
